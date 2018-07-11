775700fc3d270fbaf83bdd78937bb88a75930ad1
charliedua <duaanmol2@gmail.com>
2018-07-11 22:50:36 +1000

    Optimized Terminal User Input Library

--- ConsoleApp1/TerminalUserInput.cs
+++ ConsoleApp1/TerminalUserInput.cs
@@ -4,26 +4,13 @@ namespace TerminalUserInput
 {
 	public class TUI
 	{
-		public static int ReadInteger(string prompt)
+		public static int ReadInteger(string prompt = "")
 		{
 			int inputInt;
 			string input;
 			do
 			{
-				Console.Write(prompt);
-				input = Console.ReadLine();
-			} while (!int.TryParse(input, out inputInt));
-
-			return inputInt;
-		}
-
-		public static int ReadInteger()
-		{
-			int inputInt;
-			string input;
-			do
-			{
-				Console.Write("> ");
+				Console.Write(prompt + " >");
 				input = Console.ReadLine();
 			} while (!int.TryParse(input, out inputInt));
 
@@ -47,32 +34,19 @@ namespace TerminalUserInput
 			return input;
 		}
 
-		public static decimal ReadDecimal(string prompt)
+		public static decimal ReadDecimal(string prompt = "")
 		{
 			decimal inputDec;
 			string input;
 			do
 			{
-				Console.Write(prompt);
+				Console.Write(prompt + " >");
 				input = Console.ReadLine();
 			} while (!decimal.TryParse(input, out inputDec));
 			return inputDec;
 		}
 
-		public static decimal ReadDecimal()
-		{
-			decimal inputDec;
-			string input;
-			do
-			{
-				Console.Write("> ");
-				input = Console.ReadLine();
-			} while (!decimal.TryParse(input, out inputDec));
-
-			return inputDec;
-		}
-
-		public static bool ReadBool()
+		public static bool ReadBool(string prompt = "")
 		{
 			string input = "";
 			do
@@ -80,24 +54,6 @@ namespace TerminalUserInput
 				Console.Write("Enter Y/N> ");
 				input = Console.ReadLine();
 			} while (input == "Y" || input == "N" || input == "y" || input == "n");
-
-			if (input == "Y" || input == "y")
-			{
-				return true;
-			}
-			else
-			{
-				return false;
-			}
-		}
-		public static bool ReadBool(string prompt)
-		{
-			string input = "";
-			do
-			{
-				Console.Write(prompt + "Enter Y/N> ");
-				input = Console.ReadLine();
-			} while (!(input == "Y" || input == "N" || input == "y" || input == "n"));
 
 			if (input == "Y" || input == "y")
 			{
